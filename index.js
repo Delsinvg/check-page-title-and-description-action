e9bb8db806d0a5d2293584cef66b1efd695110d2
Delsin <devagr@hotmail.com>
2021-04-08 11:10:55 +0200

    Using axios

--- index.js
+++ index.js
@@ -1,21 +1,19 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
+const axios = require("axios").default;
 
 try {
   const siteUrl = core.getInput("site");
   let filledIn = false;
 
-  $.ajax({
-    url: siteUrl,
-    async: true,
-    success: function (data) {
-      var matches = data.match(/<title>(.*?)<\/title>/);
-      if (matches[0].length > 0) {
-        filledIn = true;
-        console.log(matches[0]);
-      }
-    },
-  });
+  axios
+    .get(siteUrl)
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
 
   core.setOutput("Title is filled in:", filledIn);
 } catch (error) {
